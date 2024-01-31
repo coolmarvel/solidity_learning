75b1883518d470b73c53f831081623664281fef9
coolmarvel <marvel97@naver.com>
2024-01-31 12:45:58 +0900

    global variable(time unit) learning & test

--- contracts/GlobalVariable.sol
+++ contracts/GlobalVariable.sol
@@ -6,4 +6,11 @@ contract GlobalVariable {
     uint public weiUnit = 1 wei; // == 1
     uint public gweiUnit = 1 gwei; // == 1e9
     uint public etherUnit = 1 ether; // == 1e18
+
+    // TIME unit
+    uint public seconsUnit = 1 seconds;
+    uint public minutesUnit = 1 minutes;
+    uint public hoursUnit = 1 hours;
+    uint public daysUnit = 1 days;
+    uint public weeksUnit = 1 weeks;
 }
--- test/GlobalVariable.ts
+++ test/GlobalVariable.ts
@@ -21,9 +21,19 @@ describe("GlobalVariable test", () => {
     it("ETH unit", async () => {
       const { variable } = await deployContract();
 
-      console.log("wei: ", (await variable.weiUnit()).toString());
-      console.log("gwei: ", (await variable.gweiUnit()).toString());
-      console.log("ether: ", (await variable.etherUnit()).toString());
+      console.log("weiUnit: ", (await variable.weiUnit()).toString());
+      console.log("gweiUnit: ", (await variable.gweiUnit()).toString());
+      console.log("etherUnit: ", (await variable.etherUnit()).toString());
+    });
+
+    it("TIME unit", async () => {
+      const { variable } = await deployContract();
+
+      console.log("seconsUnit: ", (await variable.seconsUnit()).toString());
+      console.log("minutesUnit: ", (await variable.minutesUnit()).toString());
+      console.log("hoursUnit: ", (await variable.hoursUnit()).toString());
+      console.log("daysUnit: ", (await variable.daysUnit()).toString());
+      console.log("weeksUnit: ", (await variable.weeksUnit()).toString());
     });
   });
 });
