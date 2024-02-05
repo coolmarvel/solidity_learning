e9e17041c6f6d53b582fe2b512fa95fbae22a12f
coolmarvel <marvel97@naver.com>
2024-02-05 20:16:08 +0900

    prettierrc apply for solidity extension

--- contracts/Function1.sol
+++ contracts/Function1.sol
@@ -1,35 +1,35 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 contract Function1 {
-    uint public stateValue = 30;
+  uint public stateValue = 30;
 
-    function function1() public {
-        uint localValue = 30;
-        console.log("function1 call", localValue);
-    }
+  function function1() public {
+    uint localValue = 30;
+    console.log('function1 call', localValue);
+  }
 
-    function functionParam(uint a) public {
-        console.log("a: ", a);
-    }
+  function functionParam(uint a) public {
+    console.log('a: ', a);
+  }
 
-    function publicFunction() public {
-        internalFunction();
-        this.externalFunction(); // pure 접근 제어자를 붙이면 externalFunction call 불가능
-        _privateFunction();
-    }
+  function publicFunction() public {
+    internalFunction();
+    this.externalFunction(); // pure 접근 제어자를 붙이면 externalFunction call 불가능
+    _privateFunction();
+  }
 
-    function internalFunction() internal {
-        console.log("internal call");
-    }
+  function internalFunction() internal {
+    console.log('internal call');
+  }
 
-    function externalFunction() external {
-        console.log("external call");
-    }
+  function externalFunction() external {
+    console.log('external call');
+  }
 
-    function _privateFunction() private {
-        console.log("private call");
-    }
+  function _privateFunction() private {
+    console.log('private call');
+  }
 }
--- contracts/GlobalVariable.sol
+++ contracts/GlobalVariable.sol
@@ -2,43 +2,43 @@
 pragma solidity ^0.8.9;
 
 contract GlobalVariable {
-    // ETH unit
-    uint public weiUnit = 1 wei; // == 1
-    uint public gweiUnit = 1 gwei; // == 1e9
-    uint public etherUnit = 1 ether; // == 1e18
+  // ETH unit
+  uint public weiUnit = 1 wei; // == 1
+  uint public gweiUnit = 1 gwei; // == 1e9
+  uint public etherUnit = 1 ether; // == 1e18
 
-    // TIME unit
-    uint public seconsUnit = 1 seconds;
-    uint public minutesUnit = 1 minutes;
-    uint public hoursUnit = 1 hours;
-    uint public daysUnit = 1 days;
-    uint public weeksUnit = 1 weeks;
+  // TIME unit
+  uint public seconsUnit = 1 seconds;
+  uint public minutesUnit = 1 minutes;
+  uint public hoursUnit = 1 hours;
+  uint public daysUnit = 1 days;
+  uint public weeksUnit = 1 weeks;
 
-    // Block data
-    uint public blockNumber = block.number;
-    uint public baseFee = block.basefee;
-    bytes32 public blockHash = blockhash(blockNumber);
-    bytes32 public oldBlockHash = blockhash(blockNumber - 257);
-    uint public chainId = block.chainid;
-    address public addressCoinbase = block.coinbase;
-    uint public blockDifficulty = block.prevrandao;
-    uint public gasLimit = block.gaslimit;
-    uint public blockTimestamp = block.timestamp;
+  // Block data
+  uint public blockNumber = block.number;
+  uint public baseFee = block.basefee;
+  bytes32 public blockHash = blockhash(blockNumber);
+  bytes32 public oldBlockHash = blockhash(blockNumber - 257);
+  uint public chainId = block.chainid;
+  address public addressCoinbase = block.coinbase;
+  uint public blockDifficulty = block.prevrandao;
+  uint public gasLimit = block.gaslimit;
+  uint public blockTimestamp = block.timestamp;
 
-    // Message data
-    bytes public msgData;
-    address public msgSender;
-    bytes4 public msgSig;
-    uint public msgValue;
+  // Message data
+  bytes public msgData;
+  address public msgSender;
+  bytes4 public msgSig;
+  uint public msgValue;
 
-    function msgTest(uint8 a) public payable {
-        msgData = msg.data;
-        msgSender = msg.sender;
-        msgSig = msg.sig;
-        msgValue = msg.value;
-    }
+  function msgTest(uint8 a) public payable {
+    msgData = msg.data;
+    msgSender = msg.sender;
+    msgSig = msg.sig;
+    msgValue = msg.value;
+  }
 
-    // Transaction data
-    uint public gasPrice = tx.gasprice;
-    address public origin = tx.origin;
+  // Transaction data
+  uint public gasPrice = tx.gasprice;
+  address public origin = tx.origin;
 }
